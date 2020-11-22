330cf37594472797ec0d9e4cae0ccc6b1a952a99
afischer <andreas.fischer@prodyna.com>
2020-11-22 17:48:58 +0100

    generateSW: test override config

---
+++ burst-of-buds/config-overrides.js
@@ -0,0 +1,11 @@
+const {GenerateSW} = require('workbox-webpack-plugin');
+
+module.exports = {
+    // Other webpack config...
+    plugins: [
+        // Other plugins...
+        new GenerateSW({
+            include: ['**\/*.{html,js,css,png}'],
+        })
+    ]
+};
